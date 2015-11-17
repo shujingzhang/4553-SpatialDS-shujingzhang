b8f8192bc5c73517db38a2297ee604bfce7d48bc
shujingzhang <lich.huihui@gmail.com>
2015-11-17 15:11:04 -0600

    Create load_files.py

---
+++ ProjAstar-1/load_files.py
@@ -0,0 +1,23 @@
+import csv
+import json
+
+nodes = {}
+edges = {}
+geomtry = {}
+
+with open('nodes.csv', 'rb') as csvfile:
+  rows = csv.reader(csvfile,delmiter= ',',quotechar ='""')
+  for row in rows
+    nodes.append(row)
+    
+    
+with open('edges.csv', 'rb') as csvfile:
+  rows = csv.reader(csvfile,delmiter= ',',quotechar ='""')
+  for row in rows
+    edges.append(row)
+    
+f = open ('nodegeometry.json' , 'r')    
+for line in f
+  line = json.loads(line)
+  print line('id')
+  
